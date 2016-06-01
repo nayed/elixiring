ffa9b9a50cd53528cdff4cc142d5432716f2aaff
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-06-01 16:18:09 +0200

    .

--- learning elixir/chapter 3/mymath.exs
+++ learning elixir/chapter 3/mymath.exs
@@ -4,4 +4,10 @@ defmodule MyMath do
     end
 
     defp do_square(x), do: x * x
+
+    def pow(x), do: pow(x, 2)
+
+    def pow(x, p) do
+        Enum.reduce(Enum.take(Stream.repeatedly(fn -> x end), p), &*/2)
+    end
 end
