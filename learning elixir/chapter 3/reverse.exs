1f77b000c0924e13ccbedc2d5e25cae6bf7d853f
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-06-03 11:39:01 +0200

    add test reverse

--- learning elixir/chapter 3/reverse.exs
+++ learning elixir/chapter 3/reverse.exs
@@ -1,4 +1,11 @@
 defmodule Reverse do
+  @doc """
+  Reverse a list
+
+  ## Example
+      iex> Reverse.reverse [1, 2, 3]
+      [3, 2, 1]
+  """
   def reverse([]), do: []
 
   def reverse([h|t]), do: reverse(t) ++ [h]
