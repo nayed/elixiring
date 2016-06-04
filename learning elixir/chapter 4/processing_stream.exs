39e32219503331218605ac1e2459c91a1521bc3e
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-06-04 23:00:23 +0200

    processing with stream module

---
+++ learning elixir/chapter 4/processing_stream.exs
@@ -0,0 +1,28 @@
+defmodule ProcessStream do
+    def tenFirstOdd do
+        1..100
+        |> Stream.filter(&(rem(&1, 2) != 0))
+        |> Enum.take(10)
+    end
+end
+
+
+IO.inspect ProcessStream.tenFirstOdd
+# => [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+
+
+enum_f = fn() ->
+    1..10000000
+    |> Enum.filter(&(rem(&1, 2) != 0))
+    |> Enum.take(10)
+end
+IO.inspect :timer.tc(enum_f)
+# => SO LOW
+
+stream_f = fn() ->
+    1..10000000
+    |> Stream.filter(&(rem(&1, 2) != 0))
+    |> Enum.take(10)
+end
+IO.inspect :timer.tc(stream_f)
+# => SO SO SO FAST OMG
