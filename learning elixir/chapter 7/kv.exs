48656e69e8267c845df152984aab6194dd6898da
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-06-19 10:39:39 +0200

    creating and getting processes more easily

--- learning elixir/chapter 7/kv.exs
+++ learning elixir/chapter 7/kv.exs
@@ -2,7 +2,7 @@ defmodule KV do
   use GenServer
 
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, [], opts)
+    GenServer.start_link(__MODULE__, [], [name: __MODULE__] ++ opts)
   end
 
   def init(_) do
@@ -15,5 +15,13 @@ defmodule KV do
 
   def handle_call({:get, key}, _from, dictionary) do
     {:reply, Map.get(dictionary, key), dictionary}
+  end
+
+  def put(key, value) do
+    GenServer.call(__MODULE__, {:put, key, value})
+  end
+
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
   end
 end
