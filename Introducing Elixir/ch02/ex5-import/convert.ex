54000a38f78ecfccc2a2527aa77b5a680d69d62d
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-08-03 22:50:30 +0200

    import

---
+++ Introducing Elixir/ch02/ex5-import/convert.ex
@@ -0,0 +1,5 @@
+defmodule Convert do
+  def mps_to_mph(mps), do: 2.23693629 * mps
+
+  def mps_to_kph(mps), do: 3.6 * mps
+end
---
+++ Introducing Elixir/ch02/ex5-import/drop.ex
@@ -0,0 +1,7 @@
+defmodule Drop do
+  import :math, only: [sqrt: 1]
+
+  def fall_velocity(distance) do
+    sqrt(2 * 9.8 * distance)
+  end
+end
